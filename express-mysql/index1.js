52baafe6c21e6d4b8d77aca334ff2dff564e312b
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 12:13:54 -0500

    modifications

--- express-mysql/index1.js
+++ express-mysql/index1.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.set('port', 9248);
 app.use(express.static('public'));
 
