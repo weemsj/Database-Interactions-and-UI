58bf7a21c91f85b3146e3d280745538b9a877f6c
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 00:46:48 -0500

    modifications

--- express-mysql/exercise-handlers.js
+++ express-mysql/exercise-handlers.js
@@ -1,52 +1,19 @@
 const express = require('express');
 const mysql = require('./dbcon.js');
-const req = new XMLHttpRequest()
 
 const app = express();
-const handlebars = require('express-handlebars').create({defaultLayout:'main'});
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
 app.set('port', 9248);
 app.use(express.static('public'));
 
-app.get('/',function(req,res){
-    let queryP = [];
-    for (let param in req.query){
-        queryP.push({'name':param,'value':req.query[param]})
-    }
-    let context = {};
-    context.infoList = queryP;
-    res.render('get-checker', context);
-});
+app.get('/', function (req, res){
 
-app.post('/',function(req,res){
-    let queryP = [];
-    for (let param in req.query){
-        queryP.push({'name':param,'value':req.query[param]})
-    }
-    console.log(queryP);
-    console.log(req.query);
-    let context = {};
-    context.infoList = queryP;
-    res.render('post-check', context);
-});
+})
 
-app.post('/', function(req,res){
-    let queryP = [];
-    for (let param in req.body){
-        queryP.push({'name':param,'value':req.body[param]})
-    }
-    console.log(queryP);
-    console.log(req.body);
-    let context = {};
-    context.dataList = queryP;
-    res.render('post-check', context);
-});
 app.get('/', function(req, res, next){
     let context = {};
     mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
@@ -55,19 +22,20 @@ app.get('/', function(req, res, next){
             return;
         }
         context.results = JSON.stringify(rows);
-        res.render('see', context);
+        res.send(context);
     });
 });
 
 app.get('/',function(req,res,next){
     let context = {};
-    mysql.pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.c], function(err, result){
+    mysql.pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `unit`, ) VALUES (?, ?, ?, ?, ?)",
+        [req.body.name, req.body.reps, req.body.weight, req.body.date, req.body.unit], function(err, result){
         if(err){
             next(err);
             return;
         }
-        context.results = "Inserted id " + result.insertId;
-        res.render('home',context);
+        context.results = result.insertId;
+        res.send(context);
     });
 });
 
@@ -83,7 +51,7 @@ app.get('/reset-table',function(req,res,next){
             "lbs BOOLEAN)";
         mysql.pool.query(createString, function(err){
             context.results = "Table reset";
-            res.render('exercise-table',context);
+            res.send(context);
         })
     });
 });
@@ -91,14 +59,14 @@ app.get('/reset-table',function(req,res,next){
 
 app.use(function(req,res){
     res.status(404);
-    res.render('404');
+    res.send('404');
 });
 
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('plain/text');
     res.status(500);
-    res.render('500');
+    res.send('500');
 });
 
 app.listen(app.get('port'), function(){
--- express-mysql/exercise.js
+++ express-mysql/exercise.js
@@ -1,15 +1,30 @@
-document.addEventListener("DOMContentLoaded", addExercise);
 
-function addExercise(){
-    document.getElementById('in-add').addEventListener('click', function (event){
-        let req = new XMLHttpRequest();
-        let name = document.getElementById('in-name').value;
-        let reps = document.getElementById('in-reps').value;
-        let weight = document.getElementById('in-weight').value;
-        let date = document.getElementById('date').value;
-        let unit = document.getElementById('in-unit').value;
-        if(name){
-            req.open('')
-        }
+document.getElementById('addWorkoutForm').addEventListener('submit', function (event){
+    event.preventDefault()
+
+    const formData = event.target
+    const body = JSON.stringify({
+        name: formData.elements.name.value,
+        reps: formData.elements.reps.value,
+        weight: formData.elements.reps.value,
+        date: formData.elements.date.value,
+        unit: formData.element.unit.value,
     })
-}
+
+    const headers = {
+        'content-type': 'application/json'
+    }
+    const container = document.getElementById('addWeightContainer')
+    fetch('/', {method: 'POST', body, headers})
+        .then(function (response){
+            if(response.status < 200 || response.status >= 300)
+                throw new Error(`Request failed with status ${response.status}`)
+            return response.json()
+        })
+        .then(function (json){
+            container.innerHTML = '<b> Thank you for signing up </b>'
+        })
+        .catch(function (err) {
+            container.innerHTML = `<b> We're sorry, we had a problem </b>`
+        })
+})
