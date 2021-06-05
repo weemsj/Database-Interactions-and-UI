afc7da0818627717e9ba202ae268633f449c88d3
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 11:56:35 -0500

    modifications

--- express-mysql/index.js
+++ express-mysql/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mysql = require('./dbcon.js');
-
+const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(morgan('dev'))
 app.set('port', 9248);
 app.use(express.static('public'));
 
