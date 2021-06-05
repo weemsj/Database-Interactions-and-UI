cacabfde78b0886081a7e65a709495c87c9ec211
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 11:17:31 -0500

    modifications

--- express-mysql/exercise-handlers.js
+++ express-mysql/exercise-handlers.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('port', 22);
+app.set('port', 9248);
 app.use(express.static('public'));
 
 
