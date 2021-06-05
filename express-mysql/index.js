207b689661430be9fcefe66591680341acab6506
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-05 14:57:26 -0500

    modifications

--- express-mysql/index.js
+++ express-mysql/index.js
@@ -11,8 +11,6 @@ app.use(morgan('dev'));
 app.set('port', 9248);
 app.use(express.static('public'));
 
-
-/*
 app.get('/', function(req, res, next){
     let context = {};
     mysql.pool.query('SELECT * FROM workouts', function(err, rows){
@@ -67,7 +65,7 @@ app.use(function(err, req, res, next){
     res.status(500);
     res.send('500');
 });
-*/
+
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
