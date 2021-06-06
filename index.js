722f8fcc4e7b9792c27230845659ff94a3dfd581
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-06 00:57:08 -0500

    modifications

--- index.js
+++ index.js
@@ -19,25 +19,17 @@ app.get('/', function(req, res){
     res.render('workout', {layout:null})
 })
 
+app.get('/', function (req, res){
+    const formElem = document.getElementById('addWorkoutForm')
 
-app.get('/api/exercise', function(req, res, next) {
-        let data = res.json();
-        buildTable(data);
-})
-
-function buildTable(data) {
-    let table = document.getElementById('exerciseBody')
-    for (let i = 0; i < data.length; i++) {
-        let row = `<tr>
-                        <td>${data[i].name}</td>
-                        <td>${data[i].reps}</td>
-                        <td>${data[i].weight}</td>
-                        <td>${data[i].unit}</td>
-                        <td>${data[i].date}</td>
-                   </tr>`
-        table.innerHTML += row
+    formElem.addEventListener('submit', function(event){
+        event.preventDefault()
+    })
+    let data = new FormData(formElem);
+    for(let value of data.values()) {
+        console.log(value)
     }
-}
+})
 /*
 
 app.get('/', function(req, res, next){
