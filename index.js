2fd6293413dcf01690d36d415759fef9006dabec
weemsj <71594052+weemsj@users.noreply.github.com>
2021-06-06 00:33:52 -0500

    modifications

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ app.get('/', function(req, res){
 })
 
 
+fetch('/api/exercise').then(
+    res=>{
+        const data = res.json()
+        buildTable(data)
+    }
+)
+function buildTable(data) {
+    let table = document.getElementById('exerciseBody')
+    for (let i = 0; i < data.length; i++) {
+        let row = `<tr>
+                        <td>${data[i].name}</td>
+                        <td>${data[i].reps}</td>
+                        <td>${data[i].weight}</td>
+                        <td>${data[i].unit}</td>
+                        <td>${data[i].date}</td>
+                   </tr>`
+        table.innerHTML += row
+    }
+}
 /*
 
 app.get('/', function(req, res, next){
